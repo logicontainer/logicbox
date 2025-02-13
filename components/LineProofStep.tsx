22818c90deca31d59cec43ae2aec388827ac8794
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2025-02-13 14:28:57 +0100

    Feature: Add line prepend or append to any line

--- components/LineProofStep.tsx
+++ components/LineProofStep.tsx
@@ -2,24 +2,37 @@ import "katex/dist/katex.min.css";
 
 import { LineNumberLine, LineProofStep as TLineProofStep } from "@/types/types";
 
+import { AddLineCommand } from "@/lib/commands";
 import { InlineMath } from "react-katex";
 import { Justification } from "./Justification";
 import { cn } from "@/lib/utils";
+import { useHistory } from "@/contexts/HistoryProvider";
 import { useProof } from "@/contexts/ProofProvider";
 
 export function LineProofStep ({ ...props }: TLineProofStep & { lines: LineNumberLine[] }) {
   const { lineInFocus, setLineInFocus, removeFocusFromLine } = useProof();
+  const historyContext = useHistory();
   const isInFocus = lineInFocus == props.uuid;
+  const handleAddLine = (prepend: boolean = false) => {
+    const addLineCommand = new AddLineCommand(props.uuid, prepend);
+    historyContext.addToHistory(addLineCommand)
+  }
   return (
     <div
-      className={cn("flex justify-between gap-8 text-lg/10 text-slate-800 cursor-pointer px-[-1rem]", isInFocus ? "text-highlight underline" : "")}
+      className={cn("flex relative justify-between gap-8 text-lg/10 text-slate-800 cursor-pointer px-[-1rem]", isInFocus ? "text-highlight underline" : "")}
       onMouseOver={() => setLineInFocus(props.uuid)}
       onMouseLeave={() => removeFocusFromLine(props.uuid)}
     >
+      {isInFocus && <div className="h-4 w-4 bg-red-200 absolute top-[-8px] hover:bg-red-400" onClick={() => handleAddLine(true)} onMouseOverCapture={(e) => {
+        e.stopPropagation();
+      }}></div>}
       <p className="shrink">
         <InlineMath math={props.latexFormula} />
       </p>
       <Justification justification={props.justification} lines={props.lines} />
+      {isInFocus && <div className="h-4 w-4 bg-green-200 absolute bottom-[-8px] hover:bg-green-600" onClick={() => handleAddLine(false)} onMouseOverCapture={(e) => {
+        e.stopPropagation();
+      }}></div>}
     </div>
   )
 }
--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,27 +1,13 @@
-import { AddLineCommand } from "@/lib/commands";
 import { History } from "./History";
 import { UndoRedo } from "./UndoRedo";
-import { useHistory } from "@/contexts/HistoryProvider";
 
 export function Toolbar () {
-  const historyContext = useHistory();
-
-
-  const handleAddLine = () => {
-    const addLineCommand = new AddLineCommand("j");
-    historyContext.addToHistory(addLineCommand)
-  }
-
   return (
     <div className="flex gap-4">
       <div className="flex-col">
         <UndoRedo />
         <History />
       </div>
-      <div>
-        <button className="border-solid border-2 border-slate-800 rounded-sm px-4" onClick={handleAddLine}>
-          Add line before what is initially line 8
-        </button>
-      </div></div>
+    </div>
   )
 }
