d0c509ecddf7fa4ba3f1c6c491f43e3c0d6b7662
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2025-02-25 19:09:53 +0100

    Feature: Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import { Geist, Geist_Mono } from "next/font/google";
 
+import LogicBoxSVG from "@/app/logicbox-icon.svg";
 import type { Metadata } from "next";
 import { Providers } from "@/components/Providers";
 
@@ -28,8 +29,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
       >
+        <div className="fixed top-0 left-0 w-16 h-16 m-4 lg:w-24 lg:h-24">
+          <LogicBoxSVG />
+        </div>
         <Providers>{children}</Providers>
       </body>
     </html>
