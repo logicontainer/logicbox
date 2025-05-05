055b7e7f377cd303375c5f9b8db893c52f02fe4b
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2025-05-05 20:55:06 +0200

    Feat/sidebar (#33)

--- frontend/components/ContextSidebar.tsx
+++ frontend/components/ContextSidebar.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import Card from "./Card";
+import { useLines } from "@/contexts/LinesProvider";
+import { useProof } from "@/contexts/ProofProvider";
+import { useServer } from "@/contexts/ServerProvider";
+
 export default function ContextSidebar() {
-  return <div className=" border-l-2 sm:h-screen p-2">Right sidebar</div>;
+  const { lineInFocus } = useProof();
+  const { lines } = useLines();
+  const line = lines.find((line) => line.uuid === lineInFocus);
+
+  const { proofDiagnostics } = useServer();
+  const error = proofDiagnostics.find((d) => d.uuid === lineInFocus);
+
+  const lineOrBox = line?.stepType === "box" ? "Box" : "Line";
+  return (
+    <div className="  sm:h-screen p-2">
+      {line && (
+        <Card>
+          <p className="text-left text-xl py-2">{lineOrBox} in focus</p>
+          <p>{JSON.stringify(line, null, 2)}</p>
+          {error && (
+            <>
+              <p className="text-left text-sm py-2">Error</p>
+              <p className="text-red-500">Error: {error.violationType}</p>
+            </>
+          )}
+        </Card>
+      )}
+    </div>
+  );
 }
--- frontend/components/Toolbar.tsx
+++ frontend/components/Toolbar.tsx
@@ -6,6 +6,7 @@ import {
   CaretRightIcon,
 } from "@radix-ui/react-icons";
 
+import Card from "./Card";
 import { Toolbar } from "radix-ui";
 import { Tooltip } from "react-tooltip";
 import { ValidateProofButton } from "./ValidateProofButton";
@@ -24,40 +25,42 @@ function AppToolbar() {
   };
   return (
     <div className="flex gap-2">
-      <Toolbar.Root
-        className="flex w-full min-w-max rounded-md bg-white p-2.5 shadow-[0_2px_4px] shadow-blackA4 shadow-gray-600"
-        aria-label="Formatting options"
-      >
-        <Toolbar.ToolbarButton
-          className={cn(
-            !historyContext.canUndo ? "text-slate-500 border-slate-500" : ""
-          )}
-          title="Undo latest action"
-          onClick={handleUndo}
-          disabled={!historyContext.canUndo}
+      <Card>
+        <Toolbar.Root
+          className="flex w-full min-w-max rounded-md bg-white "
+          aria-label="Formatting options"
         >
-          <CaretLeftIcon />
-        </Toolbar.ToolbarButton>
-        <Toolbar.ToolbarButton
-          className={cn(
-            !historyContext.canRedo ? "text-slate-500 border-slate-500" : ""
-          )}
-          title="Redo latest action"
-          onClick={handleRedo}
-          disabled={!historyContext.canRedo}
-        >
-          <CaretRightIcon />
-        </Toolbar.ToolbarButton>
-        <Toolbar.Separator className="mx-2.5 w-px bg-gray-600" />
-        <Toolbar.ToolbarButton
-          title="Copy or paste proof as JSON"
-          data-tooltip-id="proof-text-area"
-        >
-          <ActivityLogIcon />
-        </Toolbar.ToolbarButton>
-        <Toolbar.Separator className="mx-2.5 w-px bg-gray-600" />
-        <ValidateProofButton />
-      </Toolbar.Root>
+          <Toolbar.ToolbarButton
+            className={cn(
+              !historyContext.canUndo ? "text-slate-500 border-slate-500" : ""
+            )}
+            title="Undo latest action"
+            onClick={handleUndo}
+            disabled={!historyContext.canUndo}
+          >
+            <CaretLeftIcon />
+          </Toolbar.ToolbarButton>
+          <Toolbar.ToolbarButton
+            className={cn(
+              !historyContext.canRedo ? "text-slate-500 border-slate-500" : ""
+            )}
+            title="Redo latest action"
+            onClick={handleRedo}
+            disabled={!historyContext.canRedo}
+          >
+            <CaretRightIcon />
+          </Toolbar.ToolbarButton>
+          <Toolbar.Separator className="mx-2.5 w-px bg-gray-600" />
+          <Toolbar.ToolbarButton
+            title="Copy or paste proof as JSON"
+            data-tooltip-id="proof-text-area"
+          >
+            <ActivityLogIcon />
+          </Toolbar.ToolbarButton>
+          <Toolbar.Separator className="mx-2.5 w-px bg-gray-600" />
+          <ValidateProofButton />
+        </Toolbar.Root>
+      </Card>
       <Tooltip className="z-50" id="proof-text-area" clickable place="top">
         <textarea
           className="p-4 text-slate-800 bg-slate-200 opacity-100"
