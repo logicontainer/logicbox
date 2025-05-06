063cf4e18b292a14b5f8e2bf99d0dc0ece8e800e
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2025-05-06 16:15:15 +0200

    Improve Toolbar hitbox

--- frontend/components/Toolbar.tsx
+++ frontend/components/Toolbar.tsx
@@ -25,40 +25,47 @@ function AppToolbar() {
   };
   return (
     <div className="flex gap-2">
-      <Card>
+      <Card className="p-0">
         <Toolbar.Root
           className="flex w-full min-w-max rounded-md bg-white "
           aria-label="Formatting options"
         >
           <Toolbar.ToolbarButton
             className={cn(
-              !historyContext.canUndo ? "text-slate-500 border-slate-500" : ""
+              "py-2 pl-2",
+              historyContext.canUndo
+                ? "hover:bg-gray-100"
+                : "text-slate-500 border-slate-500 "
             )}
             title="Undo latest action"
             onClick={handleUndo}
             disabled={!historyContext.canUndo}
           >
-            <CaretLeftIcon />
+            <CaretLeftIcon className="w-8 h-8" />
           </Toolbar.ToolbarButton>
           <Toolbar.ToolbarButton
             className={cn(
-              !historyContext.canRedo ? "text-slate-500 border-slate-500" : ""
+              "py-2 pr-2",
+              historyContext.canRedo
+                ? "hover:bg-gray-100"
+                : "text-slate-500 border-slate-500 "
             )}
             title="Redo latest action"
             onClick={handleRedo}
             disabled={!historyContext.canRedo}
           >
-            <CaretRightIcon />
+            <CaretRightIcon className="w-8 h-8" />
           </Toolbar.ToolbarButton>
-          <Toolbar.Separator className="mx-2.5 w-px bg-gray-600" />
+          <Toolbar.Separator className="w-px bg-gray-600 my-3 mx-1" />
           <Toolbar.ToolbarButton
+            className="py-2 px-4 hover:bg-gray-100"
             title="Copy or paste proof as JSON"
             data-tooltip-id="proof-text-area"
           >
-            <ActivityLogIcon />
+            <ActivityLogIcon className="w-4 h-4" />
           </Toolbar.ToolbarButton>
-          <Toolbar.Separator className="mx-2.5 w-px bg-gray-600" />
-          <ValidateProofButton />
+          <Toolbar.Separator className=" w-px bg-gray-600 my-3 mx-1" />
+          <ValidateProofButton className="p-2 hover:bg-gray-100" />
         </Toolbar.Root>
       </Card>
       <Tooltip className="z-50" id="proof-text-area" clickable place="top">
--- frontend/components/ValidateProofButton.tsx
+++ frontend/components/ValidateProofButton.tsx
@@ -1,26 +1,33 @@
+import {
+  TransitionEnum,
+  useInteractionState,
+} from "@/contexts/InteractionStateProvider";
+
 import { Toolbar } from "radix-ui";
 import { UpdateIcon } from "@radix-ui/react-icons";
 import { cn } from "@/lib/utils";
-import { TransitionEnum, useInteractionState } from "@/contexts/InteractionStateProvider";
 import { useServer } from "@/contexts/ServerProvider";
 
-export function ValidateProofButton() {
-  const { doTransition } = useInteractionState()
-  const { syncingStatus } = useServer()
+export function ValidateProofButton({
+  className = "",
+}: {
+  className?: string;
+}) {
+  const { doTransition } = useInteractionState();
+  const { syncingStatus } = useServer();
 
   const handleValidateProof = () => {
-    doTransition({ enum: TransitionEnum.VALIDATE_PROOF })
+    doTransition({ enum: TransitionEnum.VALIDATE_PROOF });
   };
 
   return (
     <Toolbar.ToolbarButton
+      className={cn(className)}
       title="Validate proof by syncing with the server"
       onClick={handleValidateProof}
     >
-      <div
-        className={cn(syncingStatus == "syncing" ? "animate-spin" : "")}
-      >
-        <UpdateIcon />
+      <div className={cn(syncingStatus == "syncing" ? "animate-spin" : "")}>
+        <UpdateIcon className="h-5 w-5" />
       </div>
     </Toolbar.ToolbarButton>
   );
