e6b0d020adde87253b8cc266cf2cd44927b247de
Felix Berg <93908883+felix-berg@users.noreply.github.com>
2025-05-22 18:00:26 +0200

    feat: add CLI for testing (#56)

---
+++ backend/src/main/scala/logicbox/server/ServerMain.scala
@@ -0,0 +1,48 @@
+package logicbox.server
+
+import zio._
+import zio.http._
+
+import spray.json.JsonParser
+import logicbox.server.StandardProofValidatorService
+import zio.http.Middleware.{CorsConfig, cors}
+
+object GreetingRoutes {
+  val config: CorsConfig = CorsConfig()
+
+  def apply(): Routes[Any, Nothing] =
+    Routes(
+      // GET /greet
+      Method.GET / "greet" -> handler(Response.text(s"Hello World!")),
+
+      // GET /greet/:name
+      Method.GET / "greet" / string("name") -> handler {
+        (name: String, _: Request) =>
+          Response.text(s"Hello $name!")
+      },
+
+      Method.POST / "verify" -> handler {
+        (req: Request) => 
+          (for {
+            body <- req.body.asString
+            json = JsonParser(body)
+            res = StandardProofValidatorService().validateProof(json) match {
+              case Left(value) => Response.text(value.toString)
+              case Right(value) => Response.text(value.prettyPrint).updateHeaders(
+                headers => headers.addHeader(Header.ContentType(MediaType.application.json))
+              )
+            }
+          } yield res).catchAll(err => ZIO.succeed(Response.status(Status.InternalServerError)))
+      }
+    ) @@ cors(config)
+}
+
+// TODO: bug with List() in refs to not not intro
+
+object ServerMain extends ZIOAppDefault {
+  def run = {
+    Server
+      .serve(GreetingRoutes())
+      .provide(Server.defaultWithPort(8080))
+  }
+}
