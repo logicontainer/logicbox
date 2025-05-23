281b2874275f75a91ca6a451ccc8ac55c365e6f6
Felix Berg <93908883+felix-berg@users.noreply.github.com>
2025-05-23 13:29:46 +0200

    fix: visual bug when selecting parent as ref (#69)

--- frontend/lib/proof-step-highlight.ts
+++ frontend/lib/proof-step-highlight.ts
@@ -11,8 +11,11 @@ export enum Highlight {
 export function getStepHighlight(stepUuid: string, currentlyHoveredUuid: string | null, interactionState: InteractionState) {
   const currentlyBeingHovered = currentlyHoveredUuid === stepUuid
   const currentlySelected = getSelectedStep(interactionState) == stepUuid;
-  const otherIsEditingRef = interactionState.enum === InteractionStateEnum.EDITING_REF
-  if (currentlyBeingHovered && otherIsEditingRef) {
+  const refBeingEdited = interactionState.enum === InteractionStateEnum.EDITING_REF && interactionState.lineUuid === stepUuid
+  const otherIsEditingRef = interactionState.enum === InteractionStateEnum.EDITING_REF && interactionState.lineUuid !== stepUuid
+  if (refBeingEdited) {
+    return Highlight.NOTHING;
+  } else if (currentlyBeingHovered && otherIsEditingRef) {
     return Highlight.HOVERED_AND_OTHER_IS_SELECTING_REF;
   } else if (currentlySelected) {
     return Highlight.SELECTED;
