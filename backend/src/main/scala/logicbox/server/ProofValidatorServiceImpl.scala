67ff4af17b4c948df76d5a0eacb7340eb5ccba33
Felix Berg <93908883+felix-berg@users.noreply.github.com>
2025-05-23 18:39:46 +0200

    feat: add endpoint for pred logic (#71)

--- backend/src/main/scala/logicbox/server/ProofValidatorServiceImpl.scala
+++ backend/src/main/scala/logicbox/server/ProofValidatorServiceImpl.scala
@@ -1,39 +1,26 @@
 package logicbox.server
 
-import logicbox.framework.ProofValidatorService
-import logicbox.framework.ProofChecker
+import logicbox.framework._
 
 import spray.json._
-import logicbox.framework.Proof
-import logicbox.framework.Diagnostic
 import scala.util.Try
 import spray.json.JsonParser.ParsingException
 
+import logicbox.server.format.RawProof
 import logicbox.server.format.RawProofConverter
-import logicbox.server.format.SprayFormatters
-
-private type SprayErr = DeserializationException | SerializationException | ParsingException
 
 class ProofValidatorServiceImpl[F, R, B](
   val rawProofConverter: RawProofConverter[Proof[F, R, B, String]],
   val proofChecker: ProofChecker[F, R, B, String],
-) extends ProofValidatorService[SprayErr] {
+) extends ProofValidatorService[Unit] {
 
-  import logicbox.server.format.SprayFormatters._
-
-  private def safeSpray[T](f: => T): Either[SprayErr, T] = try {
-    Right(f)
-  } catch {
-    case e: SprayErr => Left(e)
+  override def validateProof(rawProof: RawProof): Either[Unit, ValidationResult] = {
+    val proof = rawProofConverter.convertFromRaw(rawProof)
+    val diagnostics = proofChecker.check(proof)
+    val outputRawProof = rawProofConverter.convertToRaw(proof)
+    Right(ValidationResult(
+      proof = outputRawProof,
+      diagnostics = diagnostics
+    ))
   }
-
-  override def validateProof(proofJson: JsValue): Either[SprayErr, JsValue] = for {
-    rawProof <- safeSpray { rawProofFormat.read(proofJson) }
-    proof = rawProofConverter.convertFromRaw(rawProof)
-    diagnostics = proofChecker.check(proof).map(writeDiagnostic(_))
-    outputRawProof <- safeSpray { rawProofFormat.write(rawProofConverter.convertToRaw(proof)) }
-  } yield JsObject(
-    "proof" -> outputRawProof,
-    "diagnostics" -> JsArray(diagnostics)
-  )
 }
--- backend/src/main/scala/logicbox/server/ServerMain.scala
+++ backend/src/main/scala/logicbox/server/ServerMain.scala
@@ -3,34 +3,54 @@ package logicbox.server
 import zio._
 import zio.http._
 
-import spray.json.JsonParser
+import spray.json._
 import logicbox.server.PropLogicProofValidatorService
 import zio.http.Middleware.{CorsConfig, cors}
+import logicbox.server.format.RawProof
+import logicbox.framework.ValidationResult
+import logicbox.server.format.Stringifiers
+
+case class VerifyBody(
+  proof: RawProof,
+  logicName: String,
+)
 
 object GreetingRoutes {
+  import logicbox.server.format.SprayFormatters._
   val config: CorsConfig = CorsConfig()
+
+  implicit val bodyFormat: JsonFormat[VerifyBody] = jsonFormat2(VerifyBody.apply)
+  implicit object validationResultWriter extends JsonWriter[ValidationResult] {
+    override def write(result: ValidationResult): JsValue = {
+      JsObject(
+        "proof" -> rawProofFormat.write(result.proof),
+        "diagnostics" -> JsArray(
+          result.diagnostics.map(writeDiagnostic(_))
+        )
+      )
+    }
+  }
 
   def apply(): Routes[Any, Nothing] =
     Routes(
-      // GET /greet
-      Method.GET / "greet" -> handler(Response.text(s"Hello World!")),
-
-      // GET /greet/:name
-      Method.GET / "greet" / string("name") -> handler {
-        (name: String, _: Request) =>
-          Response.text(s"Hello $name!")
-      },
-
       Method.POST / "verify" -> handler {
         (req: Request) => 
           (for {
             body <- req.body.asString
-            json = JsonParser(body)
-            res = PropLogicProofValidatorService().validateProof(json) match {
-              case Left(value) => Response.text(value.toString)
-              case Right(value) => Response.text(value.prettyPrint).updateHeaders(
-                headers => headers.addHeader(Header.ContentType(MediaType.application.json))
-              )
+            VerifyBody(proof, logicName) = bodyFormat.read(JsonParser(body))
+            validator <- logicName match {
+              case "propositionalLogic" => ZIO.succeed(PropLogicProofValidatorService())
+              case "predicateLogic" => ZIO.succeed(PredLogicProofValidatorService())
+              case _ => ZIO.fail(s"unknown logicName: $logicName")
+            }
+            res = validator.validateProof(proof) match {
+              case Left(value) => 
+                Response.text(value.toString)
+
+              case Right(result) => 
+                Response.text(validationResultWriter.write(result).prettyPrint).updateHeaders(
+                  headers => headers.addHeader(Header.ContentType(MediaType.application.json))
+                )
             }
           } yield res).catchAll(err => ZIO.succeed(Response.status(Status.InternalServerError)))
       }
