486e39667bd951a664d67d5683f1969c8b681dba
Felix Berg <93908883+felix-berg@users.noreply.github.com>
2025-05-26 15:20:21 +0200

    Fix: consistent naming of refs (#81)

--- backend/src/main/scala/logicbox/framework/RuleViolation.scala
+++ backend/src/main/scala/logicbox/framework/RuleViolation.scala
@@ -7,11 +7,11 @@ object RuleViolation {
   case object MissingRule extends RuleViolation
   case class MissingDetailInReference(refIdx: Int, expl: String) extends RuleViolation
   case class WrongNumberOfReferences(exp: Int, actual: Int, expl: String = "") extends RuleViolation
-  case class ReferenceShouldBeBox(ref: Int, expl: String = "") extends RuleViolation
-  case class ReferenceShouldBeLine(ref: Int, expl: String = "") extends RuleViolation
-  case class ReferenceDoesntMatchRule(ref: Int, expl: String = "") extends RuleViolation
+  case class ReferenceShouldBeBox(refIdx: Int, expl: String = "") extends RuleViolation
+  case class ReferenceShouldBeLine(refIdx: Int, expl: String = "") extends RuleViolation
+  case class ReferenceDoesntMatchRule(refIdx: Int, expl: String = "") extends RuleViolation
   case class ReferencesMismatch(refs: List[Int], expl: String = "") extends RuleViolation
-  case class FormulaDoesntMatchReference(refs: Int, expl: String = "") extends RuleViolation
+  case class FormulaDoesntMatchReference(refIdx: Int, expl: String = "") extends RuleViolation
   case class FormulaDoesntMatchRule(expl: String = "") extends RuleViolation
   case class MiscellaneousViolation(expl: String = "") extends RuleViolation
 }
