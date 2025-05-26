eef0af6e158cb4c1e45175e135cbeb08dfeef897
Felix Berg <felixbpedersen@gmail.com>
2025-05-26 15:19:39 +0200

    work

--- backend/src/main/scala/logicbox/framework/Diagnostic.scala
+++ backend/src/main/scala/logicbox/framework/Diagnostic.scala
@@ -7,8 +7,8 @@ sealed trait Diagnostic[+Id] {
 object Diagnostic {
   case class RuleViolationAtStep[Id](stepId: Id, violation: RuleViolation) extends Diagnostic[Id]
   case class StepNotFound[Id](stepId: Id) extends Diagnostic[Id]
-  case class ReferenceIdNotFound[Id](stepId: Id, whichRef: Int, refId: Id) extends Diagnostic[Id]
-  case class MalformedReference[Id](stepId: Id, whichRef: Int, refId: Id, expl: String) extends Diagnostic[Id]
+  case class ReferenceIdNotFound[Id](stepId: Id, refIdx: Int, refId: Id) extends Diagnostic[Id]
+  case class MalformedReference[Id](stepId: Id, refIdx: Int, refId: Id, expl: String) extends Diagnostic[Id]
 
   case class ReferenceToLaterStep[+Id](stepId: Id, refIdx: Int, refId: Id) extends Diagnostic[Id]
   case class ScopeViolation[+Id](stepId: Id, stepScope: Scope[Id], refIdx: Int, refId: Id, refScope: Scope[Id]) extends Diagnostic[Id]
