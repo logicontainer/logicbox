fb1b1145cfe78a72c5dd1f0e0dc082266c5aaf8e
Felix Berg <felixbpedersen@gmail.com>
2025-07-07 16:33:53 +0200

    add proof navigator

--- backend/src/main/scala/logicbox/proof/ProofNavigator.scala
+++ backend/src/main/scala/logicbox/proof/ProofNavigator.scala
@@ -7,8 +7,40 @@ import logicbox.framework.Location
 class ProofNavigator[F, B, Id, O](
   formulaNavigator: Navigator[F, O],
   boxInfoNavigator: Navigator[B, O],
-) extends Navigator[Proof[F, ?, B, Id], O] {
-  override def get(proof: Proof[F, ?, B, Id], loc: Location): Option[O] = {
-    ???
+) extends Navigator[(Proof[F, ?, B, Id], Id), O] {
+  private enum Result {
+    case Nothing
+    case FoundFormula(formula: F, rest: Location)
+    case FoundBoxInfo(info: B, rest: Location)
+  }
+
+  private def getFormula(proof: Proof[F, ?, B, Id], stepId: Id, loc: Location): Result =
+    proof.getStep(stepId) match {
+      case Right(Proof.Line(formula, _, _)) => 
+        Result.FoundFormula(formula, loc)
+
+      case Right(Proof.Box(info, ids)) => loc.steps match {
+        case 0 :: rest => ids.headOption
+          .map(getFormula(proof, _, Location(rest)))
+          .getOrElse(Result.Nothing)
+
+        case 1 :: rest => ids.lastOption
+          .map(getFormula(proof, _, Location(rest)))
+          .getOrElse(Result.Nothing)
+
+        case 2 :: rest => Result.FoundBoxInfo(info, Location(rest))
+
+        case _ => Result.Nothing
+      }
+
+      case _ => Result.Nothing
+    }
+
+  override def get(proofAndId: (Proof[F, ?, B, Id], Id), loc: Location): Option[O] = {
+    getFormula(proofAndId._1, proofAndId._2, loc) match {
+      case Result.FoundFormula(formula, remaining) => formulaNavigator.get(formula, remaining)
+      case Result.FoundBoxInfo(info, remaining) => boxInfoNavigator.get(info, remaining)
+      case Result.Nothing => None
+    }
   }
 }
---
+++ backend/src/test/scala/logicbox/proof/ProofNavigatorTest.scala
@@ -0,0 +1,122 @@
+package logicbox.proof
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.*
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.Inspectors
+import logicbox.framework.Navigator
+import logicbox.framework.Location
+
+class ProofNavigatorTest extends AnyFunSpec {
+  import logicbox.ProofStubs._
+  describe("get") {
+    val nav = ProofNavigator[F, B, Id, String](
+      new Navigator[StubFormula, String] {
+        override def get(subject: StubFormula, loc: Location): Option[String] = 
+          Some(s"${subject.i.toString}_${loc.steps.map(_.toString).mkString}")
+      },
+      new Navigator[StubBoxInfo, String] {
+        override def get(subject: StubBoxInfo, loc: Location): Option[String] = 
+          Some(s"${subject.info}_${loc.steps.map(_.toString).mkString}")
+      }
+    )
+
+    it("should get formula of single line") {
+      val pf = StubProof(
+        rootSteps = Seq("1"),
+        map = Map(
+          "1" -> StubLine(StubFormula(10))
+        )
+      )
+
+      nav.get((pf, "1"), Location.root) shouldBe Some("10_")
+    }
+
+    it("should get assumption of box") {
+      val pf = StubProof(
+        rootSteps = Seq("box", "ass"),
+        map = Map(
+          "ass" -> StubLine(StubFormula(40)),
+          "box" -> StubBox(info = StubBoxInfo(), steps = Seq("ass"))
+        )
+      )
+
+      nav.get((pf, "box"), Location.assumption) shouldBe Some("40_")
+    }
+
+    it("should get conclusion of first step of box") {
+      val pf = StubProof(
+        rootSteps = Seq("outer", "inner", "ass"),
+        map = Map(
+          "ass" -> StubLine(StubFormula()),
+          "concl" -> StubLine(StubFormula(30)),
+          "inner" -> StubBox(info = StubBoxInfo(), steps = Seq("ass", "concl")),
+          "outer" -> StubBox(info = StubBoxInfo(), steps = Seq("inner")),
+        )
+      )
+
+      nav.get((pf, "outer"), Location.assumption.conclusion) shouldBe Some("30_")
+    }
+
+    it("should get deep nested boxes") {
+      val pf = StubProof(
+        rootSteps = Seq("b1", "b2", "b3", "line"),
+        map = Map(
+          "line" -> StubLine(StubFormula(123)),
+          "b1" -> StubBox(StubBoxInfo(), Seq("line")),
+          "b2" -> StubBox(StubBoxInfo(), Seq("b1")),
+          "b3" -> StubBox(StubBoxInfo(), Seq("b2"))
+        )
+      )
+
+      nav.get((pf, "b3"), Location.assumption.conclusion.assumption) shouldBe Some("123_")
+    }
+
+    it("should obtain box info") {
+      val pf = StubProof(
+        rootSteps = Seq("box"),
+        map = Map(
+          "box" -> StubBox(info = StubBoxInfo("yessir"))
+        )
+      )
+
+      nav.get((pf, "box"), Location.freshVar) shouldBe Some("yessir_")
+    }
+
+    it("should let rest of location through to box info") {
+      val pf = StubProof(
+        rootSteps = Seq("box"),
+        map = Map(
+          "box" -> StubBox(info = StubBoxInfo("thing"))
+        )
+      )
+
+      nav.get((pf, "box"), Location.freshVar.operand(4)) shouldBe Some("thing_4")
+    }
+
+    it("should let rest of location through to formula") {
+      val pf = StubProof(
+        rootSteps = Seq("1"),
+        map = Map(
+          "1" -> StubLine(StubFormula(123))
+        )
+      )
+
+      nav.get((pf, "1"), Location.lhs.rhs.lhs) shouldBe Some("123_010")
+    }
+
+    it("should return none on invalid id") {
+      val pf = StubProof()
+      nav.get((pf, "bruh"), Location.root) shouldBe None
+    }
+
+    it("should return none if location is invalid") {
+      val pf = StubProof(
+        rootSteps = Seq("box"),
+        map = Map("box" -> StubBox())
+      )
+
+      nav.get((pf, "box"), Location.operand(3)) shouldBe None
+    }
+  }
+}
