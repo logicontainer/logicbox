78caaf7bbae7a6d9ff03355b55341f34e7b0b4a0
Kasper Skov Hansen <56992010+kasperskovhansen@users.noreply.github.com>
2025-07-14 22:54:34 +0200

    upload proof (#98)

--- frontend/app/gallery/Gallery.tsx
+++ frontend/app/gallery/Gallery.tsx
@@ -3,6 +3,7 @@ import { useProofStore } from "@/store/proofStore";
 import GalleryItem from "./GalleryItem";
 import { ProofWithMetadata } from "@/types/types";
 import NewProofButton from "./NewProofButton";
+import UploadProofButton from "@/components/UploadProofButton";
 
 export default function Gallery() {
   const proofs = useProofStore((state) => state.proofs);
@@ -12,8 +13,9 @@ export default function Gallery() {
       <div className="flex gap-2 px-4">
         <p className="text text-2xl font-bold">My proofs</p>
         <NewProofButton />
+        <UploadProofButton />
       </div>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 w-full p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 w-full p-4">
         {proofs.map((proof: ProofWithMetadata) => {
           return <GalleryItem key={proof.id} proof={proof} />;
         })}
