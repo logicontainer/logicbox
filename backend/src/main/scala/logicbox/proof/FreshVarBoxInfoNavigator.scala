73fc9d50ff3c67e0b285ae249e63a9e8508e2837
Felix Berg <felixbpedersen@gmail.com>
2025-07-24 13:45:43 +0200

    work

--- backend/src/main/scala/logicbox/proof/FreshVarBoxInfoNavigator.scala
+++ backend/src/main/scala/logicbox/proof/FreshVarBoxInfoNavigator.scala
@@ -3,10 +3,11 @@ package logicbox.proof
 import logicbox.framework.Navigator
 import logicbox.rule.FreshVarBoxInfo
 import logicbox.framework.Location
+import logicbox.framework.Location.Step
 
 class FreshVarBoxInfoNavigator[V] extends Navigator[FreshVarBoxInfo[V], V] {
   override def get(subject: FreshVarBoxInfo[V], loc: Location): Option[V] = loc.steps match {
-    case Nil => subject.freshVar
+    case Step.FreshVar :: Nil => subject.freshVar
     case _ => None
   }
 }
