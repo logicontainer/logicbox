9bbb41a83b18bc4b812eeba2cc10e84158b1b497
Felix Berg <felixbpedersen@gmail.com>
2025-07-24 14:09:40 +0200

    work

--- frontend/types/types.ts
+++ frontend/types/types.ts
@@ -82,6 +82,7 @@ export type Diagnostic = { uuid: UUID } & (
   | { errorType: "MissingRef", refIdx: number }
 
   | { errorType: "PremiseInsideBox" }
+  | { errorType: "FreshVarEscaped", boxId: string, freshVar: string }
 
   | { errorType: "ReferenceOutOfScope", refIdx: number }
   | { errorType: "ReferenceToLaterStep", refIdx: number }
