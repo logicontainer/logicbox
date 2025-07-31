ab627c183f0e5d105e9393ef3e239e51bce1d1b6
Felix Berg <felixbpedersen@gmail.com>
2025-07-31 22:14:02 +0200

    work

---
+++ electron/compile.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+cd backend
+echo "------------ COMPILING BACKEND"
+
+sbt fullLinkJS
+cd ..
+
+
+echo "------------ MOVING OUTPUT FILE TO FRONTEND"
+
+SBTOUTPUTFILE="backend/target/scala-3.4.3/root-opt/main.js"
+DSTFILE="frontend/public/logicbox_backend.js"
+
+echo "\
+// This file was generated during a build of this commit:
+// - current commit hash: $(git rev-parse HEAD)
+// - origin URL: $(git remote get-url origin)
+// - link: $(git remote get-url origin | sed -E "s/(git@|https:\/\/)(github.com|gitlab.com)[:\/](.*)\.git/https:\/\/\2\/\3\/commit\/$(git rev-parse HEAD)/")" \
+  > $DSTFILE
+
+cat $SBTOUTPUTFILE >> $DSTFILE
+
+cd frontend
+echo "------------ RUNNING pnpm build"
+pnpm build
