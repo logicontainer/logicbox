9c4e4f445af5f4d9f5c83e4f194589d8c647f8e4
Felix Berg <93908883+felix-berg@users.noreply.github.com>
2025-07-31 20:18:15 +0200

    Revert "fix: set export to output in frontend (#169)"
    This reverts commit 701aa5cdb48c79e869c5a3be844b256b869f7370.

--- electron/index.js
+++
@@ -1,9 +0,0 @@
-import { app, BrowserWindow } from 'electron/main'
-import serve from 'electron-serve'
-
-serve({ directory: 'app', file: "gallery" })
-
-app.whenReady().then(async () => {
-  const win = new BrowserWindow()
-  await win.loadURL("app://-")
-})
