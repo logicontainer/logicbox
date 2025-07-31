f6650cde231497e10b4820a2ef1aec2f073cb69c
Felix Berg <felixbpedersen@gmail.com>
2025-07-31 22:47:43 +0200

    finish 'prepare' script

--- electron/prepare.sh
+++ electron/prepare.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+if [ "$(basename "$PWD")" != "electron" ]; then
+  echo "should only be run from the 'electron' directory"
+  exit 1
+fi
+
+rm -rf app
+
 cd ../backend
 echo "------------ COMPILING BACKEND"
 
+./clear_buildfiles.sh
 sbt fullLinkJS
 cd ..
-
 
 echo "------------ MOVING OUTPUT FILE TO FRONTEND"
 
@@ -21,11 +28,16 @@ echo "\
 
 cat $SBTOUTPUTFILE >> $DSTFILE
 
+echo "------------ TOP 10 LINES OF GENERATED JS FILE"
+cat $DSTFILE | head -n 10
+
+cd frontend
 echo "------------ RUNNING pnpm build"
+rm -rf out
 NEXT_OUTPUT_MODE=export pnpm build
 
 cd ..
 echo "------------ COPYING pnpm OUTPUT TO electron/app"
-cp -rv frontend/out electron/app
+cp -rv frontend/out electron/app | grep "logicbox_backend.js" --colour=always # just to see the generated js file
 
 cd electron
