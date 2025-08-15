16655d150a70a6cf3b65be6652acd998c4f0c14f
Felix Berg <felixbpedersen@gmail.com>
2025-08-15 23:03:57 +0200

    make gallery dynamic (idk... definitely not server-side renderable tho)

--- frontend/app/gallery/page.tsx
+++ frontend/app/gallery/page.tsx
@@ -1,5 +1,12 @@
-import GalleryPage from "@/components/GalleryPage";
+"use client"
+
+// app/gallery/page.tsx
+import dynamic from 'next/dynamic';
+
+const GalleryPage = dynamic(() => import('@/components/GalleryPage'), {
+  ssr: false,
+});
 
 export default function Page() {
-  return <GalleryPage/>
+  return <GalleryPage />;
 }
