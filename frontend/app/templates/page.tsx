9b3844a28695614fb8d93e42eb3196a20eb73b4c
Felix Berg <felixbpedersen@gmail.com>
2026-02-04 17:12:01 +0100

    made t dynamic to stop ssr bugs

--- frontend/app/templates/page.tsx
+++ frontend/app/templates/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import dynamic from "next/dynamic"
+
 import { useProofStore } from "@/store/proofStore";
 import { ProofWithMetadata } from "@/types/types";
 import { useSearchParams } from "next/navigation";
@@ -8,39 +10,43 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import jsonTemplates from "@/public/templates.json"
-const templates: ProofWithMetadata[] = jsonTemplates as ProofWithMetadata[]
+const templates = jsonTemplates as ProofWithMetadata[]
 
-export default function Page() {
+function Page() {
   const params = useSearchParams()
   const proofs = useProofStore(state => state.proofs)
+  const store = useProofStore()
   const addProof = useProofStore((state) => state.addProof);
   const router = useRouter()
-  const templateId = params.get("id")
-  if (templateId === null) {
-    alert("no template id given")
-    return;
-  }
 
-  const proofAlreadyThere = proofs.find(proof => proof.id.startsWith(templateId))
-  if (proofAlreadyThere) {
-    React.useEffect(() => {
-      router.push(`/proof?id=${proofAlreadyThere.id}`)
-    })
-    return `Redirecting to ${proofAlreadyThere.id}`;
-  }
-  
-  const template = templates.find(t => t.id === templateId)
-  if (template === undefined) {
-    alert(`unknown template "${templateId}"`)
-    return;
-  }
+  const templateId = params.get("id")
 
   React.useEffect(() => {
-    template.id = template.id + uuidv4()
-    template.createdAt = new Date().toISOString()
-    addProof(template)
-    router.push(`/proof/?id=${template.id}`);
-  })
+    if (templateId === null) {
+      alert("no template id given")
+      return;
+    }
 
-  return `Creating new proof with id ${template.id}`;
+    const proofAlreadyThere = proofs.find(proof => proof.id.startsWith(templateId))
+    if (proofAlreadyThere) {
+      alert("You already have a proof based on this template. You will be redirected to it.")
+      router.push(`/proof?id=${proofAlreadyThere.id}`)
+    } else {
+      const template = templates.find(t => t.id === templateId)
+      if (template) {
+        template.id = template.id + uuidv4()
+        template.createdAt = new Date().toISOString()
+        addProof(template)
+        router.push(`/proof/?id=${template.id}`);
+      } else {
+        alert(`No template with id ${templateId}`)
+      }
+    }
+  }, [])
+
+  return `Loading template ${templateId}`
 }
+
+export default dynamic(() => Promise.resolve(Page), {
+  ssr: false
+})
