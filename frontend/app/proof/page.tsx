dbae6684be698aff42b21b7879b62abb00613b81
Felix Berg <felixbpedersen@gmail.com>
2026-02-06 10:31:43 +0100

    work

--- frontend/app/proof/page.tsx
+++ frontend/app/proof/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import ProofEditorPage from "@/components/ProofEditorPage";
+import { InteractionStateEnum, TransitionEnum, useInteractionState } from "@/contexts/InteractionStateProvider";
 import { useSearchParams } from "next/navigation";
+import React from "react";
 import { Suspense } from "react";
 
 function PageImpl() {
@@ -15,6 +17,23 @@ function PageImpl() {
 }
 
 export default function Page() {
+  const { doTransition } = useInteractionState()
+  React.useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if ((e.ctrlKey || e.metaKey) && !e.shiftKey && e.key === "z") {
+        e.preventDefault()
+        doTransition({ enum: TransitionEnum.UNDO })
+      } else if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.key === "z") {
+        e.preventDefault()
+        doTransition({ enum: TransitionEnum.REDO })
+      }
+    }
+
+    document.addEventListener('keydown', listener, { capture: true })
+
+    return () => document.removeEventListener('keydown', listener, { capture: true })
+  }, [])
+
   return <Suspense> {/* needed so next doesn't just fucking ignore "use client" */}
     <PageImpl />
   </Suspense>
