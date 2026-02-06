286c71caaedb1b83737ea82e1801c3dbf49dbc11
Felix Berg <felixbpedersen@gmail.com>
2026-02-06 14:45:16 +0100

    work

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -5,10 +5,15 @@ export default function Footer() {
     <footer className="w-full">
       <div className="max-w-screen mx-auto py-2 px-4">
         <p className="text-center text-gray-600">
-          Made with <span className="text-red-500">❤️</span> by{" "}
-          <Link href="https://logicbox.dk" className="text-blue-500">
-            LogicBox.dk
+          Made by{" "}
+          <Link href="https://kasperskov.dev" className="text-blue-500">
+            Kasper Skov Hansen
           </Link>
+          {" "}and{" "}
+          <Link href="https://github.com/felix-berg" className="text-blue-500">
+            Felix Berg
+          </Link>
+          {" "} @ Aarhus University
           {/* &copy; {new Date().getFullYear()} All rights reserved. Made with{" "}
           <span className="text-red-500">❤️</span> by{" "}
           <Link href="LogicBox.dk">LogicBox.dk</Link> */}
