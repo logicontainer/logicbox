c3216f4fff99359d820feee4dc6c76124e57fbe7
Felix Berg <93908883+felix-berg@users.noreply.github.com>
2026-02-12 22:05:52 +0100

    fix: inline math elements (#274)

--- frontend/components/MemoizedInlineMath.tsx
+++ frontend/components/MemoizedInlineMath.tsx
@@ -1,5 +1,25 @@
+import { useProof } from "@/contexts/ProofProvider";
 import "katex/dist/katex.min.css";
 import React from "react";
-import { InlineMath } from "react-katex";
+import { InlineMath, MathComponentPropsWithMath } from "react-katex";
 
-export const MemoizedInlineMath = React.memo(InlineMath)
+const IN_FISH_MODE = (localStorage.getItem("mode") === "FISH");
+(window as any).toggleFishMode = () => {
+  if (IN_FISH_MODE) {
+    localStorage.removeItem("mode")
+  } else {
+    localStorage.setItem("mode", "FISH")
+  }
+  window.location.href = window.location.href
+}
+
+const MyInlineMath = (props: MathComponentPropsWithMath) => {
+  if (IN_FISH_MODE) {
+    const math = props.math
+      .replaceAll("x_0", "🐟")
+      .replaceAll("y_0", "🐠")
+    return <InlineMath {...props} math={math}/>
+  } else return <InlineMath {...props}/>
+}
+
+export const MemoizedInlineMath = React.memo(MyInlineMath)
